efb30974c4923c1db4df75b3c1895e90d237d7e1
DiogoMeneses-stack <diogomeneses0605xyz@gmail.com>
2022-07-20 08:00:09 -0300

    Project send

---
+++ src/main/java/com/diogomeneses/DiogomenesesStackApplication.java
@@ -0,0 +1,13 @@
+package com.diogomeneses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiogomenesesStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiogomenesesStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogomeneses/DiogomenesesStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogomeneses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiogomenesesStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
